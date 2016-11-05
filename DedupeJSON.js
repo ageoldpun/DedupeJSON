3fde06535522a4ca0840577b587c7cdde85b31cb
Paul Ogden <ageoldpun@gmail.com>
2016-11-05 13:21:11 -0700

    MVP

--- DedupeJSON.js
+++ DedupeJSON.js
@@ -1,13 +1,74 @@
 var fs = require('fs');
-var _ = require('./dependencies/lodash');
 
 /*
  * REQUIREMENTS
  * 1. The data from the newest date should be preferred
- * 2. duplicate IDs count as dups. Duplicate emails count as dups. Both must be unique in our dataset. Duplicate values elsewhere do not count as dups.
+ * 2. Duplicate IDs count as dups.
+ *    Duplicate emails count as dups. Both must be unique in our dataset.
+ *    Duplicate values elsewhere do not count as dups.
  * 3. If the dates are identical the data from the record provided last in the list should be preferred
  */
 
-var leads = JSON.parse(fs.readFileSync(process.argv[2], 'utf8')).leads;
+var maxDate = function(dateA, dateB) {
+  var parsedDateA = Date.parse(dateA);
+  var parsedDateB = Date.parse(dateB);
+  if (parsedDateA > parsedDateB) {
+    return 'a';
+  } else if (parsedDateB > parsedDateA) {
+    return 'b';
+  } else if (parsedDateA == parsedDateB) {
+    return 'same';
+  }
+};
 
-console.log(leads);
+var originalLeads = JSON.parse(fs.readFileSync(process.argv[2], 'utf8')).leads;
+var returnLeads = originalLeads
+
+originalLeads.forEach(function(lead, index) {
+  // loop through the leads to see if any ids match
+  originalLeads.forEach(function(matchingLead, matchingIndex) {
+    if (lead._id === matchingLead._id && index !== matchingIndex) {
+      // if so, find out which one has later date and delete the other
+      var greaterDate = maxDate(lead.entryDate, matchingLead.entryDate);
+      if (greaterDate === 'a') {
+        // delete b from the returnLeads array
+        returnLeads.splice(returnLeads.indexOf(matchingLead), 1);
+      } else if (greaterDate === 'b') {
+        // delete a from the returnLeads array
+        returnLeads.splice(returnLeads.indexOf(lead), 1);
+      } else if (greaterDate === 'same') {
+        // find out which has higher index and delete the other from returnLeads array
+        if (originalLeads.indexOf(matchingLead) > originalLeads.indexOf(lead)) {
+          returnLeads.splice(returnLeads.indexOf(lead), 1);
+        } else {
+          returnLeads.splice(returnLeads.indexOf(matchingLead), 1);
+        }
+      }
+    }
+  });
+
+  // loop through the leads to see if any emails match
+  originalLeads.forEach(function(matchingLead, matchingIndex) {
+    if (lead.email === matchingLead.email && index !== matchingIndex) {
+      // if so, find out which one has later date and delete the other
+      var greaterDate = maxDate(lead.entryDate, matchingLead.entryDate);
+      if (greaterDate === 'a') {
+        // delete b from the returnLeads array
+        returnLeads.splice(returnLeads.indexOf(matchingLead), 1);
+      } else if (greaterDate === 'b') {
+        // delete a from the returnLeads array
+        returnLeads.splice(returnLeads.indexOf(lead), 1);
+      } else if (greaterDate === 'same') {
+        // find out which has higher index and delete the other from returnLeads array
+        if (originalLeads.indexOf(matchingLead) > originalLeads.indexOf(lead)) {
+          returnLeads.splice(returnLeads.indexOf(lead), 1);
+        } else {
+          returnLeads.splice(returnLeads.indexOf(matchingLead), 1);
+        }
+      }
+    }
+  });
+});
+
+console.log(returnLeads);
+return returnLeads;
